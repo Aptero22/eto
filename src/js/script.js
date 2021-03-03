1a3bd1b4e254adfa04b24efe7115c2c8029d209d
Stanislav <staslapa22@gmail.com>
2021-03-03 19:55:06 +0700

    home_page

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,21 @@
 $(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: $(".slick-prev"),
+        nextArrow: $(".slick-next"),
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: true,
+                    dots: false
+                }
+            },
+        ]
+    });
+
+
     $('[data-modal=new_review]').on('click', function() {
         $('.overlay, #new_review').fadeIn('slow');
     });
