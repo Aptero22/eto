d8a82cfee823e883b15892e825323f652d8be3ea
Stanislav <staslapa22@gmail.com>
2021-03-22 00:31:18 +0700

    22032021

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
+        autoplay: true,
+        autoplaySpeed: 3000,
         adaptiveHeight: true,
         prevArrow: $(".slick-prev"),
         nextArrow: $(".slick-next"),
@@ -22,5 +24,27 @@ $(document).ready(function(){
     $('.modal__top-close').on('click', function() {
         $('.overlay, #new_review').fadeOut('slow');
     });
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__top-close').on('click', function() {
+        $('.overlay, #consultation').fadeOut('slow');
+    });
+
+    function valideForms(form) {
+            $(form).validate({
+            rules: {
+                phone: "required",
+            },
+            messages: {
+                phone: "Введите номер телефона"
+            }
+        });
+    };
+
+    valideForms('#consultation form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
 });
 
